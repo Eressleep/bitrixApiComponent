29c6889dcdd7fc7e23ca1ab27ec5da64ca1c3791
Alexsander Semenov <alexsander.semenov.ser@bk.ru>
2021-05-30 18:19:15 +0300

    get-Favorites-Id-Items

--- favorites/class.php
+++ favorites/class.php
@@ -1,11 +1,46 @@
 <?php
 class favorites extends CBitrixComponent
 {
+
+	private function checkUser()
+	{
+		$data = $user = \Bitrix\Main\UserTable::getList([
+			'filter' => ['=ID' => $this->arParams['id'],],
+			'select' => ['ID','UF_FAVORIT'],
+			'cache'  =>
+				[
+					'ttl'         => 3600,
+					'cache_joins' => true,
+				],
+
+		])->fetchObject();;
+		if($data)
+		{
+			$this->arResult['status'] = 'success';
+			$this->arResult['dataObj'] = $data;
+			return true;
+		}
+		$this->arResult['error']  = "Invalid user id {$this->arParams['id']}";
+		$this->arResult['status'] = 'fail';
+		return false;
+	}
+
+	private function getFavorites()
+	{
+		if($this->checkUser())
+		{
+			$this->arResult['data'] = $this->arResult['dataObj']->getUfFavorit();
+
+		}
+		unset($this->arResult['dataObj']);
+	}
+
 	public function executeComponent()
 	{
 
-
-		$this->arResult = [1];
+		$this->getFavorites();
+		if($this->arParams['json'])
+			$this->arResult = json_encode($this->arResult);
 		$this::IncludeComponentTemplate();
 	}
 }
